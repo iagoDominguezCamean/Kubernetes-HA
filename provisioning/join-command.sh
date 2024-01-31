471e81e1c57aca14afdd5852d51d923f576d5663
iagoDominguezCamean <72920871+iagoDominguezCamean@users.noreply.github.com>
2024-01-31 12:40:05 +0100

    Segundo nodo CP conectado, tercero da error

--- provisioning/join-command.sh
+++ provisioning/join-command.sh
@@ -1,1 +1,1 @@
-kubeadm join idc-cluster-endpoint:6443 --token aymbjh.t7j5ehwbldm9nne5 --discovery-token-ca-cert-hash sha256:ecfed857412c12066d6956f34a8a2e20d38813e4d60386439046f99d732df2dc 
+kubeadm join idc-cluster-endpoint:6443 --token frkl54.xlf59zxw5tw0ko6g --discovery-token-ca-cert-hash sha256:f2764c468dbf37bedefa1321cb88fad34e2514d6e86010c0b035778f1385276a 
